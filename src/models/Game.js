ae1dccee02bde00c1bb3c1376e1129edf8ac11c3
kmsheng <kmsh3ng@gmail.com>
2018-12-27 14:20:59 +0800

    implement pill effect

--- src/models/Game.js
+++ src/models/Game.js
@@ -2,8 +2,20 @@ import {Scheduler, Observable, interval, timer, fromEvent} from 'rxjs';
 import {repeat, takeWhile, map, pluck, filter, throttleTime} from 'rxjs/operators';
 import Ship from './Ship';
 import Ball from './Ball';
+import Pill from './Pill';
 import now from './../helpers/now';
 import bricksArr from './../fixtures/bricks';
+import shuffle from './../helpers/shuffle';
+
+import {
+  EFFECT_SHIP_WIDEN,
+  EFFECT_SHIP_SHRINK,
+  EFFECT_BALL_ENLARGE,
+  EFFECT_BALL_SHRINK,
+  EFFECT_BALL_THROUGH,
+  EFFECT_FAST_SPEED,
+  EFFECT_SLOW_SPEED
+} from './../constants';
 
 class Game {
 
@@ -20,12 +32,34 @@ class Game {
     this.startedTime = 0;
     this.pausedTime = 0;
     this.wonTime = 0;
+    this.effectStartedAt = 0;
+    this.effect = null;
 
     this.ship = new Ship();
     this.ball = new Ball();
-    this.bricks = bricksArr[this.level - 1].slice();
+
+    this.setBricks();
+
+    this.pills = [];
 
     this.init();
+  }
+
+  setBricks() {
+    this.bricks = bricksArr[this.level - 1].slice();
+    const breakableBricks = this.bricks.filter(brick => brick.isBreakable);
+    const indices = Array.from({length: breakableBricks.length}, (_, i) => i);
+
+    // make 5% of bricks to contain pills
+    const length = Math.ceil(breakableBricks.length * 0.05, 10);
+    const shuffledIndices = shuffle(indices);
+    shuffledIndices.length = length;
+    this.bricks = this.bricks.map((brick, index) => {
+      if (shuffledIndices.includes(index)) {
+        brick.hasPill = true;
+      }
+      return brick;
+    });
   }
 
   start() {
@@ -128,9 +162,16 @@ class Game {
   clearBricks(indices) {
 
     indices.forEach(i => {
-      if (this.bricks[i].isBreakable) {
+      const brick = this.bricks[i];
+
+      if (brick.isBreakable) {
         this.bricks.splice(i, 1)
         this.score += 40;
+
+        if (brick.hasPill) {
+          const {x, y} = brick;
+          this.pills.push(new Pill(x, y + (brick.height / 2)));
+        }
       }
     });
   }
@@ -163,7 +204,7 @@ class Game {
     this.startedTime = 0;
     this.wonTime = 0;
     this.level += 1;
-    this.bricks = bricksArr[this.level - 1].slice();
+    this.setBricks();
   }
 
   reset() {
@@ -175,8 +216,53 @@ class Game {
     this.wonTime = 0;
     this.lostTime = 0;
     this.level = 1;
-    this.bricks = bricksArr[this.level - 1].slice();
     this.ship.isBallSticked = true;
+    this.pills.length = 0;
+    this.setBricks();
+  }
+
+  clearEffect() {
+    this.effect = null;
+    this.effectStartedAt = 0;
+    this.ship.restoreToDefaultWidth();
+    this.ball.restoreToDefaultRadius();
+  }
+
+  clearEffectIfNeeded() {
+    if ((now() - this.effectStartedAt) > 15) {
+      this.clearEffect();
+    }
+  }
+
+  applyPillEffect() {
+
+    this.clearEffect();
+
+    const effects = [
+      EFFECT_SHIP_WIDEN,
+      EFFECT_SHIP_SHRINK,
+      EFFECT_BALL_ENLARGE,
+      EFFECT_BALL_SHRINK,
+      EFFECT_BALL_THROUGH,
+      EFFECT_FAST_SPEED,
+      EFFECT_SLOW_SPEED
+    ];
+
+    this.effect = shuffle(effects)[0];
+    this.effectStartedAt = now();
+
+    if (this.effect === EFFECT_SHIP_WIDEN) {
+      this.ship.width = this.ship.width * 2.5;
+    }
+    if (this.effect === EFFECT_SHIP_SHRINK) {
+      this.ship.width = this.ship.width * 0.5;
+    }
+    if (this.effect === EFFECT_BALL_ENLARGE) {
+      this.ball.radius = this.ball.radius * 1.5;
+    }
+    if (this.effect === EFFECT_BALL_SHRINK) {
+      this.ball.radius = this.ball.radius * 0.5;
+    }
   }
 }
 
