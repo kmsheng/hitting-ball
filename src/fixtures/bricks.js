66175698d8fa685c1ce112c31f137544689eff76
kmsheng <kmsh3ng@gmail.com>
2018-12-27 14:42:15 +0800

    update level 2's bricks

--- src/fixtures/bricks.js
+++ src/fixtures/bricks.js
@@ -71,28 +71,64 @@ export default [
   ],
 
   [
-    {x: 144, y: 96, width: 30, height: 20, color: '#e84393', isBreakable: true},
-    {x: 224, y: 96, width: 30, height: 20, color: '#e84393', isBreakable: true},
+    {x: 24, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 64, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 104, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 144, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 184, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 224, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 264, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 304, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 344, y: 40, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 144, y: 96, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 224, y: 96, width: 30, height: 20, color: '#778beb', isBreakable: true},
 
+    {x: 42, y: 68, width: 30, height: 20, color: '#fd79a8', isBreakable: true},
+    {x: 82, y: 68, width: 30, height: 20, color: '#ff7675', isBreakable: true},
+    {x: 282, y: 68, width: 30, height: 20, color: '#81ecec', isBreakable: true},
+    {x: 322, y: 68, width: 30, height: 20, color: '#55efc4', isBreakable: true},
+
+
+    {x: 64, y: 124, width: 30, height: 20, color: '#778beb', isBreakable: true},
     {x: 122, y: 124, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 162, y: 124, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 202, y: 124, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 242, y: 124, width: 30, height: 20, color: '#e84393', isBreakable: true},
+    {x: 304, y: 124, width: 30, height: 20, color: '#778beb', isBreakable: true},
 
+
+    {x: 64, y: 152, width: 30, height: 20, color: '#778beb', isBreakable: true},
     {x: 144, y: 152, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 184, y: 152, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 224, y: 152, width: 30, height: 20, color: '#e84393', isBreakable: true},
+    {x: 304, y: 152, width: 30, height: 20, color: '#778beb', isBreakable: true},
 
+    {x: 42, y: 180, width: 30, height: 20, color: '#63cdda', isBreakable: true},
     {x: 82, y: 180, width: 30, height: 20, color: '#34495e', isBreakable: false},
     {x: 162, y: 180, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 202, y: 180, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 282, y: 180, width: 30, height: 20, color: '#34495e', isBreakable: false},
+    {x: 322, y: 180, width: 30, height: 20, color: '#63cdda', isBreakable: true},
 
+    {x: 24, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 64, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 104, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
     {x: 144, y: 208, width: 30, height: 20, color: '#34495e', isBreakable: false},
     {x: 184, y: 208, width: 30, height: 20, color: '#e84393', isBreakable: true},
     {x: 224, y: 208, width: 30, height: 20, color: '#34495e', isBreakable: false},
+    {x: 264, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 304, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
+    {x: 344, y: 208, width: 30, height: 20, color: '#778beb', isBreakable: true},
 
-    {x: 184, y: 238, width: 30, height: 20, color: '#34495e', isBreakable: false}
+    {x: 24, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 64, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 104, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 144, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 184, y: 238, width: 30, height: 20, color: '#34495e', isBreakable: false},
+    {x: 224, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 264, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 304, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
+    {x: 344, y: 238, width: 30, height: 20, color: '#63cdda', isBreakable: true},
   ]
 
 ];
