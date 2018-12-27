7faf4bece72b777b5938b904a79a35c9fc7b5f32
kmsheng <kmsh3ng@gmail.com>
2018-12-27 12:50:53 +0800

    rename ball -> item

--- src/helpers/detectRectCollision.js
+++ src/helpers/detectRectCollision.js
@@ -1,35 +1,35 @@
-export default function detectRectCollision(ball, rect) {
+export default function detectRectCollision(item, rect) {
 
-  const {x, y} = ball.pos;
-  const {radius} = ball;
+  const {x, y} = item.pos;
+  const {radius} = item;
 
-  const ballLeft = x - radius;
-  const ballRight = x + radius;
-  const ballTop = y - radius;
-  const ballBottom = y + radius;
+  const itemLeft = x - radius;
+  const itemRight = x + radius;
+  const itemTop = y - radius;
+  const itemBottom = y + radius;
 
   const rectTop = rect.y;
   const rectBottom = rect.y + rect.height;
   const rectLeft = rect.x;
   const rectRight = rect.x + rect.width;
 
-  if ((ballTop <= rectBottom) && (ballTop >= rectTop) &&
-    (ballLeft >= rectLeft) && (ballRight <= rectRight)) {
+  if ((itemTop <= rectBottom) && (itemTop >= rectTop) &&
+    (itemLeft >= rectLeft) && (itemRight <= rectRight)) {
     return 'TOP';
   }
 
-  if ((ballLeft <= rectRight) && (ballLeft >= rectLeft) &&
-    (ballTop >= rectTop) && (ballBottom <= rectBottom)) {
+  if ((itemLeft <= rectRight) && (itemLeft >= rectLeft) &&
+    (itemTop >= rectTop) && (itemBottom <= rectBottom)) {
     return 'LEFT';
   }
 
-  if ((ballBottom <= rectBottom) && (ballBottom >= rectTop) &&
-    (ballLeft >= rectLeft) && (ballRight <= rectRight)) {
+  if ((itemBottom <= rectBottom) && (itemBottom >= rectTop) &&
+    (itemLeft >= rectLeft) && (itemRight <= rectRight)) {
     return 'BOTTOM';
   }
 
-  if ((ballRight >= rectLeft) && (ballRight <= rectRight) &&
-    (ballTop >= rectTop) && (ballBottom <= rectBottom)) {
+  if ((itemRight >= rectLeft) && (itemRight <= rectRight) &&
+    (itemTop >= rectTop) && (itemBottom <= rectBottom)) {
     return 'RIGHT';
   }
   return '';
